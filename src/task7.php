65f90fe22fbc85ee72f996e87b9b3ca090bf1e45
Sanchez AG <ppalex.g@gmail.com>
2024-04-16 15:07:41 +0300

    initial commit

---
+++ src/task7.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+//1. Виведіть на екран всі числа від 1 до 10 використовуючи цикл while
+
+$counter = 1;
+
+while($counter <= 10) {
+    echo $counter.PHP_EOL;
+    $counter++;
+}
+
+//2. Обчисліть факторіал числа 5 використовуючи цикл while.
+
+//$i = 5;
+//$factorial = 1;
+//while ($i > 1) {
+//
+//    $factorial *= $i;
+//    $i--;
+//}
+//
+//echo $factorial.PHP_EOL;
+
+//3. Виведіть на екран всі парні числа від 1 до 20 використовуючи цикл while.
+
+//$i = 1;
+//
+//while ($i <= 20) {
+//    if($i % 2 === 0 ){
+//        echo $i.PHP_EOL;
+//    }
+//
+//    $i++;
+//}
+
