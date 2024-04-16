85f9e61219eb911faaaad5516aa96c5c962ea84f
Sanchez AG <ppalex.g@gmail.com>
2024-04-16 22:57:54 +0300

    remake task

--- src/task6.php
+++ src/task6.php
@@ -2,25 +2,32 @@
 
 declare(strict_types=1);
 
-$value1 = 1;
+$value1 = 2;
 $value2 = 3;
+
 
 function myFunc(int|float $a, int|float $b, ?closure $c = null): int|float
 {
-    if(null !== $c){
-       $c($a, $b);
+    $result = $a * $b;
+    if (null !== $c) {
+        $c($result);
     }
-    return $a * $b;
-};
+    return $result;
+}
 
-$result = function (int|float $_a, int|float $_b): void
-{
-   echo $_a * $_b.PHP_EOL;
+
+
+$printArg = function (int|float $_a): void {
+    echo $_a.PHP_EOL;
 
 };
 
 myFunc($value1, $value2).PHP_EOL;
-myFunc($value1, $value2, $result).PHP_EOL;
+myFunc($value1, $value2, $printArg);
+
+
+
+
 
 
 
