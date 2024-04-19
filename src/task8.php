d1aa24261a461ccf1de07d20be613bbd1e1cbd67
Sanchez AG <ppalex.g@gmail.com>
2024-04-19 15:34:48 +0300

    second commit

--- src/task8.php
+++ src/task8.php
@@ -2,33 +2,38 @@
 
 declare(strict_types=1);
 //Створити пустий масив і заповнити його випадковими значеннями. (створити функцію як на уроці)
-$initArray = [];
 
-for ($i = 0; $i <= 5; $i++) {
-    $initArray [$i] = rand(1, 6);
-
+function randomArray(int $length = 6, int $min = 1, int $max = 6): array
+{
+    $initArray = [];
+    for ($i = 0; $i < $length; $i++) {
+        $initArray[] = rand($min, $max);
+    }
+    return $initArray;
 }
-print_r($initArray);
+
+$generatedArray = randomArray();
+print_r($generatedArray);
 
 // Порахувати суму елементів масиву.
 $sum = 0;
-foreach ($initArray as $elem) {
+foreach ($generatedArray as $elem) {
     $sum += $elem;
 }
 echo $sum . PHP_EOL;
 
 //Порахувати добуток всіх елементів масиву.
 $multiplication = 1;
-$arrayLength = count($initArray);
+$arrayLength = count($generatedArray);
 
 for ($i = 0; $i < $arrayLength; $i++) {
-    $multiplication *= $initArray[$i];
+    $multiplication *= $generatedArray[$i];
 }
 echo $multiplication . PHP_EOL;
 
 //Перевірте скільки раз число 5 зустрічається у вас в масиві.
 $countTimes = 0;
-foreach ($initArray as $elem) {
+foreach ($generatedArray as $elem) {
     if ($elem !== 5)
         continue;
     $countTimes++;
@@ -41,8 +46,8 @@ $uniqNumbers = [];
 $resultUnique = null;
 
 for ($i = 0; $i < $arrayLength; $i++) {
-    if ($initArray[$i] % 3 === 0) {
-        $uniqNumbers[] = $initArray[$i];
+    if ($generatedArray[$i] % 3 === 0) {
+        $uniqNumbers[] = $generatedArray[$i];
 
     }
 }
@@ -53,7 +58,7 @@ if (count($uniqNumbers) === 0) {
 
 }
 
-print_r ($resultUnique) . PHP_EOL;
+print_r($resultUnique) . PHP_EOL;
 
 
 
