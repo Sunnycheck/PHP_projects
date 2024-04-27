a3330ab0b32f805245155856f91ad558cd69117f
Sanchez AG <ppalex.g@gmail.com>
2024-04-27 14:22:30 +0300

    initial commit

---
+++ src/InputFunc.php
@@ -0,0 +1,49 @@
+<?php
+//Task11
+declare(strict_types=1);
+$path = "log.txt";
+
+$keyword = 'n';
+$line = '';
+
+
+$input = [];
+while (true) {
+
+    echo "Input your data. To cancel input type 'n'." . PHP_EOL;
+
+    $line = trim(fgets(STDIN));
+    if ($line === $keyword) {
+        break;
+    }
+    $input[] = $line;
+
+
+}
+function addData(array $line, string $path): void
+{
+    $opened = fopen($path, "a+");
+
+    foreach ($line as $value) {
+        fwrite($opened, $value . PHP_EOL);
+    }
+
+    fclose($opened);
+
+}
+
+addData($input, $path);
+function readData(string $file): string
+{
+    $opened = fopen($file, 'r');
+    $read = file($file);
+    $result = end($read);
+    fclose($opened);
+    return $result;
+
+}
+
+
+
+$lastArg = readData($path);
+echo "Last input: $lastArg";
